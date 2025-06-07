d56ed4af359b34cb0493600b07054cf8026c8bec
Hamed Karayi <youritman_110@yahoo.com>
2025-06-07 13:16:32 +0330

    انتقال کپی‌رایت به پایین صفحه و اعمال فونت وزیر

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,22 @@ import RegisterForm from "../components/RegisterForm";
 
 export default function Home() {
   return (
-    <main className="min-h-screen bg-gray-50 flex flex-col items-center justify-center px-4">
-     <h1 className="text-4xl font-bold text-center mb-4 lalezar-font">کسب‌وکارت رو نخ کن</h1>
+    <>
+      <main className="min-h-screen bg-gray-50 flex flex-col items-center justify-center px-4">
+        <h1 className="text-4xl font-bold text-center mb-4 lalezar-font">
+          کسب‌وکارت رو نخ کن
+        </h1>
 
-<p className="text-lg text-center text-gray-400 mb-8 max-w-xl vazir-font">
-  اگر نخ از تولید تا پوشاک سیستمی منظم و هوشمند داشته باشه، غوغا میشه
-</p>
-<footer className="text-center text-sm text-gray-500 py-4 border-t mt-16">
-  © {new Date().getFullYear()} .تمامی حقوق متعلق به پلتفرم نخ‌نت است
-</footer>
+        <p className="text-lg text-center text-gray-700 mb-8 max-w-xl vazir-font">
+          اگر نخ از تولید تا پوشاک سیستمی منظم و هوشمند داشته باشه، غوغا میشه
+        </p>
 
-      <RegisterForm />
-    </main>
+        <RegisterForm />
+      </main>
+
+      <footer className="text-center text-sm text-gray-500 py-4 border-t mt-10 vazir-font">
+        © {new Date().getFullYear()} . تمامی حقوق متعلق به پلتفرم نخ‌نت است.
+      </footer>
+    </>
   );
 }
