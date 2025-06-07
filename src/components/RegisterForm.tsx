197a9a76e10ab76031005d1e7d93ef13d4c4531f
Hamed Karayi <youritman_110@yahoo.com>
2025-06-07 11:10:26 +0330

    Fix handleChange to support textarea

--- src/components/RegisterForm.tsx
+++ src/components/RegisterForm.tsx
@@ -31,7 +31,9 @@ export default function RegisterForm() {
     "دیگر ",
   ];
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+  const handleChange = (
+  e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement | HTMLTextAreaElement>
+) => {
   setFormData({ ...formData, [e.target.name]: e.target.value });
 };
 
