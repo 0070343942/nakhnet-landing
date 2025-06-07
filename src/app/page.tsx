dd7b7190f9d004894a5a4cdd254c1265861a474f
Hamed Karayi <youritman_110@yahoo.com>
2025-06-07 13:00:39 +0330

    افزودن متن کپی‌رایت به صفحه اصلی

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,9 +5,13 @@ export default function Home() {
     <main className="min-h-screen bg-gray-50 flex flex-col items-center justify-center px-4">
      <h1 className="text-4xl font-bold text-center mb-4 lalezar-font">کسب‌وکارت رو نخ کن</h1>
 
-<p className="text-lg text-center text-gray-700 mb-8 max-w-xl vazir-font">
-  اگر نخ از تولید تا پوشاک سیستمی منظم و هوشمند داشته باشه، غوغا میشه.
+<p className="text-lg text-center text-gray-400 mb-8 max-w-xl vazir-font">
+  اگر نخ از تولید تا پوشاک سیستمی منظم و هوشمند داشته باشه، غوغا میشه
 </p>
+<footer className="text-center text-sm text-gray-500 py-4 border-t mt-16">
+  © {new Date().getFullYear()} .تمامی حقوق متعلق به پلتفرم نخ‌نت است
+</footer>
+
       <RegisterForm />
     </main>
   );
